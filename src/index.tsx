7ead5af4e8e4f2472f9552a52140d2e96cffd7fd
小楼 <xuolou@163.com>
2025-01-20 10:02:58 +0800

    update project

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './views/App'
-
-const root = createRoot(document.querySelector("#root") as Element);
-
-root.render(<App></App>);
--- vite.preload.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-import {resolve} from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-// https://vitejs.dev/config
-export default defineConfig({});
